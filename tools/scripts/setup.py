90d8ed34cefa78ae225d9aa43e6e6f2480ab4b60
o_strokachuk <o_strokachuk@wargaming.net>
2018-02-16 22:01:28 +0300

    Generate test protobuf project

--- tools/scripts/setup.py
+++ tools/scripts/setup.py
@@ -1,4 +1,4 @@
-from os import mkdir
+from os import mkdir, chdir, getcwd
 from os.path import join
 from subprocess import run
 from shutil import rmtree
@@ -6,18 +6,27 @@ from shutil import rmtree
 from env import BUILD, REPO
 import generate_proto
 
+
+def _recreate(d):
+    try:
+        rmtree(d)
+    except:
+        pass
+
+    _mkdir(d)
+
+
+def _mkdir(d):
+    try:
+        mkdir(d)
+    except:
+        pass
+
+
 def main():
     generate_proto.main()
 
-    try:
-        rmtree(BUILD)
-    except:
-        pass
-
-    try:
-        mkdir(BUILD)
-    except:
-        pass
+    _recreate(BUILD)
 
     # python
     venv = join(BUILD, 'PYTHON_ENV')
@@ -25,7 +34,17 @@ def main():
     run(['pip', 'install', '-r', join(REPO, 'src', 'py', 'requirements.txt')])
 
     # cpp
-    run(['cmake', '-G', 'Xcode', join(REPO, 'src', 'cpp')])
+    cwd = getcwd()
+    try:
+        src = join(REPO, 'src', 'cpp')
+
+        build_ = join(REPO, '_build', 'cpp')
+        _recreate(build_)
+        chdir(build_)
+
+        run(['cmake', '-G', 'Xcode', src])
+    finally:
+        chdir(cwd)
 
 if __name__ == '__main__':
     main()
